9d475d9a7fa0c066d29197e7a44a9ea0906fc160
KlausLoeffelmann <klaus.loeffelmann@microsoft.com>
2022-04-18 15:04:16 -0700

    Add AssemblyFullyQualifiedName to test sample.

--- src/TileRepeater/TileRepeater.Controls.Simplyfied/FooBarControl.cs
+++ src/TileRepeater/TileRepeater.Controls.Simplyfied/FooBarControl.cs
@@ -4,10 +4,12 @@ namespace TileRepeater.Controls.Simplified
 {
     public class FooBarControl : Control
     {
-        [Editor("System.Drawing.Design.ColorEditor", "System.Drawing.Design.UITypeEditor")]
+        [Editor("System.Drawing.Design.ColorEditor, System.Drawing.Design, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a",
+            "System.Drawing.Design.UITypeEditor, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")]
         public Color SpecialColor { get; set; }
 
-        [Editor("System.Windows.Forms.Design.FileNameEditor", "System.Drawing.Design.UITypeEditor")]
+        [Editor("System.Windows.Forms.Design.FileNameEditor, System.Design, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a",
+                "System.Drawing.Design.UITypeEditor, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a")]
         public string? Filename { get; set; }
     }
 }
