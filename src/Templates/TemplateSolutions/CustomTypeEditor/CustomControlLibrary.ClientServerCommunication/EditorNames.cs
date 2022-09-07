be5d1ed6128376f23eba57eb6456448e5a579c6b
Klaus Loeffelmann <klaus.loeffelmann@microsoft.com>
2022-09-06 17:38:37 -0700

    Prepare project for template generation (type naming, reformatting, template.json)

---
+++ src/Templates/TemplateSolutions/CustomTypeEditor/CustomControlLibrary.ClientServerCommunication/EditorNames.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomControlLibrary.ClientServerCommunication
+{
+    /// <summary>
+    ///  Static class for holding the names of all custom type editors to provide IntelliSense support.
+    /// </summary>
+    public static class EditorNames
+    {
+        public const string CustomTypeEditor = nameof(CustomTypeEditor);
+    }
+}
